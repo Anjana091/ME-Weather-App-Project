69191b5119c4e0147b75bfb175a9fed046260812
Anjana091 <anjanasuresh091@gmail.com>
2023-05-25 17:21:22 +0530

    added api

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
-
+const api = {
+    key: "fcc8de7015bbb202209bbf0261babf4c",
+    base: "https://api.openweathermap.org/data/2.5/"
+  }
+  
 
 const searchbox = document.querySelector('.search-box');
 searchbox.addEventListener('keypress', setQuery);
