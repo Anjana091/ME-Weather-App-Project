2c384cb85161081ca840f8a159a21f0e27c4240b
Anjana091 <anjanasuresh091@gmail.com>
2023-05-28 20:58:33 +0530

    script updated

--- main.js
+++ main.js
@@ -1,51 +1,29 @@
-const api = {
-    key: "fcc8de7015bbb202209bbf0261babf4c",
-    base: "https://api.openweathermap.org/data/2.5/"
-  }
-  
+const temp = document.getElementById("temp"),
+  date = document.getElementById("date-time"),
+  condition = document.getElementById("condition"),
+  rain = document.getElementById("rain"),
+  mainIcon = document.getElementById("icon"),
+  currentLocation = document.getElementById("location"),
+  uvIndex = document.querySelector(".uv-index"),
+  uvText = document.querySelector(".uv-text"),
+  windSpeed = document.querySelector(".wind-speed"),
+  sunRise = document.querySelector(".sun-rise"),
+  sunSet = document.querySelector(".sun-set"),
+  humidity = document.querySelector(".humidity"),
+  visibilty = document.querySelector(".visibilty"),
+  humidityStatus = document.querySelector(".humidity-status"),
+  airQuality = document.querySelector(".air-quality"),
+  airQualityStatus = document.querySelector(".air-quality-status"),
+  visibilityStatus = document.querySelector(".visibilty-status"),
+  searchForm = document.querySelector("#search"),
+  search = document.querySelector("#query"),
+  celciusBtn = document.querySelector(".celcius"),
+  fahrenheitBtn = document.querySelector(".fahrenheit"),
+  tempUnit = document.querySelectorAll(".temp-unit"),
+  hourlyBtn = document.querySelector(".hourly"),
+  weekBtn = document.querySelector(".week"),
+  weatherCards = document.querySelector("#weather-cards");
 
-const searchbox = document.querySelector('.search-box');
-searchbox.addEventListener('keypress', setQuery);
-
-function setQuery(evt) {
-  if (evt.keyCode == 13) {
-    getResults(searchbox.value);
-  }
-}
-
-function getResults (query) {
-  fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
-    .then(weather => {
-      return weather.json();
-    }).then(displayResults);
-}
-
-function displayResults (weather) {
-  let city = document.querySelector('.location .city');
-  city.innerText = `${weather.name}, ${weather.sys.country}`;
-
-  let now = new Date();
-  let date = document.querySelector('.location .date');
-  date.innerText = dateBuilder(now);
-
-  let temp = document.querySelector('.current .temp');
-  temp.innerHTML = `${Math.round(weather.main.temp)}<span>°c</span>`;
-
-  let weather_el = document.querySelector('.current .weather');
-  weather_el.innerText = weather.weather[0].main;
-
-  let hilow = document.querySelector('.hi-low');
-  hilow.innerText = `${Math.round(weather.main.temp_min)}°c / ${Math.round(weather.main.temp_max)}°c`;
-}
-
-function dateBuilder (d) {
-  let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-  let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-
-  let day = days[d.getDay()];
-  let date = d.getDate();
-  let month = months[d.getMonth()];
-  let year = d.getFullYear();
-
-  return `${day} ${date} ${month} ${year}`;
-}
+let currentCity = "";
+let currentUnit = "c";
+let hourlyorWeek = "week";
